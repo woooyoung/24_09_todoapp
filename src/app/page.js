90378d7a741d92002a5adfee68b5641a9688f88f
woooyoung <axdsw121@gmail.com>
2024-10-14 11:38:12 +0900

    삭제, 수정에 아이콘 추가

--- src/app/page.js
+++ src/app/page.js
@@ -14,11 +14,13 @@ import {
   List,
   ListItem,
   Divider,
+  ListItemButton,
   Box,
 } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import classNames from 'classnames';
-import { FaBars, FaCheck, FaEllipsisV } from 'react-icons/fa';
+import { FaBars, FaCheck, FaEllipsisH, FaTrash, FaEllipsisV } from 'react-icons/fa';
+import { FaPenToSquare } from 'react-icons/fa6';
 import dateToStr from './dateUtil';
 import RootTheme from './theme';
 
@@ -148,15 +150,21 @@ function useTodoOptionDrawerStatus() {
 function TodoOptionDrawer({ status }) {
   return (
     <>
-      <SwipeableDrawer anchor="left" open={status.opened} onClose={status.close}>
+      <SwipeableDrawer anchor="top" open={status.opened} onClose={status.close} onOpen={() => {}}>
         <List>
           <ListItem className="tw-flex tw-gap-2 tw-p-[15px]">
             <span className="tw-text-[--mui-color-primary-main]">{status.todoId}번 </span>
             <span>Your Todo</span>
           </ListItem>
           <Divider className="tw-my-[5px]" />
-          <ListItem className="tw-p-[15px_20px]">수정</ListItem>
-          <ListItem className="tw-p-[15px_20px]">삭제</ListItem>
+          <ListItemButton className="tw-p-[15px_20px] tw-flex tw-gap-2 tw-items-center">
+            <span>수정</span>
+            <FaPenToSquare className="block tw-mt-[-5px]" />
+          </ListItemButton>
+          <ListItemButton className="tw-p-[15px_20px] tw-flex tw-gap-2 tw-items-center">
+            <span>삭제</span>
+            <FaTrash className="block tw-mt-[-5px]" />
+          </ListItemButton>
         </List>
       </SwipeableDrawer>
     </>
